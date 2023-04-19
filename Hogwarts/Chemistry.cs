413669e8c8af7f6dee05bcfa6b21cc765dfc8cae
Mobin <mobin.aranii@gmail.com>
2023-04-19 11:23:37 +0430

    chemistry class

---
+++ Hogwarts/Chemistry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    internal class Chemistry : Lesson
+    {
+        private List<string> _chemistryStuffList = new List<string>();
+    }
+}
