6c00e7c0553febaf4eff6a34096ea4e9b7205a10
Mobin <mobin.aranii@gmail.com>
2023-04-19 11:18:55 +0430

    lesson class defined

---
+++ Hogwarts/Lesson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    internal class Lesson
+    {
+        private string _name;
+        private TimeOnly _time;
+        private int _studentsCount;
+        private int _capacity;
+        private int _termPresentation
+
+    }
+}
