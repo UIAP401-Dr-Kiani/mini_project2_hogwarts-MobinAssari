c761021ca87e88b26ff8f108dc8e4a492630bc68
Mobin <mobin.aranii@gmail.com>
2023-04-19 15:39:14 +0430

    spell class defined

---
+++ Hogwarts/SpellLesson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    internal class SpellLesson : Lesson
+    {
+        private List<string> _spells = new List<string>();
+    }
+}
