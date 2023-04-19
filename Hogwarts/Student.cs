9e37ffb29a8c572269f3709e8163ec71f457ac5a
Mobin <mobin.aranii@gmail.com>
2023-04-19 10:55:25 +0430

    student class defined

--- Hogwarts/Student.cs
+++ Hogwarts/Student.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hogwarts
 {
-    internal class Student
+    internal class Student : AuthorizedPerson
     {
+        private String _unitPassed;
+        private String _term;
+        private String _roomNumber;
     }
 }
