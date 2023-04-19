804c6b94b03ef8008457f4331dbd197b53575290
Mobin <mobin.aranii@gmail.com>
2023-04-19 10:45:57 +0430

    group class defines

--- Hogwarts/Group.cs
+++ Hogwarts/Group.cs
@@ -8,5 +8,12 @@ namespace Hogwarts
 {
     internal class Group
     {
+        private enum _house
+        {
+            Hufflepuff, Gryffindor, Ravenclaw, Slytherin
+        }
+        private int _point;
+        private List<Student> _studentList;
+        private List<Student> _quidditchList;
     }
 }
