876e4209d42ee45d1730530df583b752c38f9265
Mobin <mobin.aranii@gmail.com>
2023-04-19 11:05:44 +0430

    prof and dorm define (last method remains)

--- Hogwarts/Dormitory.cs
+++ Hogwarts/Dormitory.cs
@@ -8,5 +8,13 @@ namespace Hogwarts
 {
     internal class Dormitory
     {
+        private enum _house
+        {
+            Hufflepuff, Gryffindor, Ravenclaw, Slytherin
+        }
+        private int _floor;
+        private int _room;
+        private int _bed;
+        private string _gender;
     }
 }
